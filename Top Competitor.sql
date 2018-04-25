2386b86789b8d6d7c4b2ac7757978d133f747d80
Aviral Kumar <31638760+akumar911@users.noreply.github.com>
2018-04-25 18:32:34 -0400

    Add files via upload

---
+++ Top Competitor.sql
@@ -0,0 +1,9 @@
+select h.hacker_id, h.name from hackers as h 
+join submissions as s
+on h.hacker_id = s.hacker_id
+join challenges as c on s.challenge_id = c.challenge_id
+join difficulty as d on c.difficulty_level = d.difficulty_level
+where s.score = d.score 
+group by h.name , h.hacker_id
+having count(*) > 1
+order by count(*) desc, h.hacker_id asc
